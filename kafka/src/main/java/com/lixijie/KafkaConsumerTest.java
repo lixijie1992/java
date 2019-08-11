227d16c8d83027ac556a1df3de784d87aca5abc5
lxj <1554125677@qq.com>
2019-08-11 19:13:43 +0800

    kafkademo-20190811

--- kafka/src/main/java/com/lixijie/KafkaConsumerTest.java
+++ kafka/src/main/java/com/lixijie/KafkaConsumerTest.java
@@ -5,9 +5,6 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.serialization.StringDeserializer;
-
-import java.time.Duration;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.Properties;
 
