9bf818123a435df01cae96b9a701c39558bdba46
lxj <1554125677@qq.com>
2019-08-11 19:26:17 +0800

    kafkademo-20190811

--- kafka/src/main/java/com/lixijie/App.java
+++ kafka/src/main/java/com/lixijie/App.java
@@ -2,13 +2,17 @@ package com.lixijie;
 
 /**
  * Hello world!
- *
+ *测试断言
+ * 开启方式：
+ * 在VM中加 -ea
  */
 public class App 
 {
     public static void main( String[] args )
     {
 
+        boolean isOk = 1 > 2;
+        assert isOk :"程序错误";
         System.out.println( "Hello World!" );
     }
 }
