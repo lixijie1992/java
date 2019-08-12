24e796fd156f103472d962d9d6739ccc5df4ebf4
lxj <1554125677@qq.com>
2019-08-12 22:57:22 +0800

    java面试练习-20190812

--- interview/src/com/lixijie/demo/SingletonPattern.java
+++ interview/src/com/lixijie/demo/SingletonPattern.java
@@ -1,7 +1,8 @@
 package com.lixijie.demo;
 
 /**
- *
+ * @author lxj
+ * @datetime
  */
 public class SingletonPattern {
 
@@ -11,7 +12,29 @@ public class SingletonPattern {
         System.out.println(Thread.currentThread().getName() + "------------------------");
     }
 
-    public static SingletonPattern getInstance() {
+
+
+    /*懒汉模式
+    * 对方法加锁影响效率*/
+ /*   public static synchronized  SingletonPattern getInstance(){
+        if (SingletonPattern==null) {
+            SingletonPattern = new SingletonPattern();
+        }
+        return SingletonPattern;
+    }*/
+
+    /*饿汉模式
+    * 需要单独写一个类*/
+   /*public class Singleton {
+    private static Singleton instance = new Singleton();
+    private Singleton (){}
+    public static Singleton getInstance() {
+    return instance;
+    }
+}*/
+
+    /*双检锁/双重校验锁*/
+   /* public static SingletonPattern getInstance() {
         if (SingletonPattern == null) {
             synchronized (SingletonPattern.class) {
                 if (SingletonPattern == null) {
@@ -20,17 +43,35 @@ public class SingletonPattern {
             }
         }
         return SingletonPattern;
-    }
+    }*/
 
     public static void main(String[] args) {
-        for (int i = 0; i < 20; i++) {
+        /*双检锁/双重校验锁*/
+     /*   for (int i = 0; i < 20; i++) {
             new Thread() {
                 @Override
                 public void run() {
                     SingletonPattern.getInstance();
                 }
             }.start();
-        }
+        }*/
+
+     /*懒汉模式*/
+       /* for (int i = 0; i < 20; i++) {
+            new Thread() {
+                @Override
+                public void run() {
+                    SingletonPattern.getInstance();
+                }
+            }.start();
+        }*/
+       /*饿汉模式*/
+
+
     }
+
+
+
+
 
 }
